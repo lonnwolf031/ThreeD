ef1562319a477599557038aee7417a1433c8d06e
Kolkman, Lonneke (LE) <lonneke.kolkman@politie.nl>
2025-11-06 15:38:33 +0100

    ~

--- application/src/main/java/lonnwolf/threed/application/gui/MainUI.java
+++ application/src/main/java/lonnwolf/threed/application/gui/MainUI.java
@@ -1,11 +1,36 @@
 package lonnwolf.threed.application.gui;
 
+import models.Triangle;
+import models.Vertex;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
 
 public class MainUI {
 
     public static JFrame mainFrame() {
+        ArrayList<Triangle> tris = new ArrayList<Triangle>();
+        tris.add(new Triangle(new Vertex(100, 100, 100),
+                new Vertex(-100, -100, 100),
+                new Vertex(-100, 100, -100),
+                Color.WHITE));
+        tris.add(new Triangle(new Vertex(100, 100, 100),
+                new Vertex(-100, -100, 100),
+                new Vertex(100, -100, -100),
+                Color.RED));
+        tris.add(new Triangle(new Vertex(-100, 100, -100),
+                new Vertex(100, -100, -100),
+                new Vertex(100, 100, 100),
+                Color.GREEN));
+        tris.add(new Triangle(new Vertex(-100, 100, -100),
+                new Vertex(100, -100, -100),
+                new Vertex(-100, -100, 100),
+                Color.BLUE));
+
+
         JFrame frame = new JFrame();
         Container pane = frame.getContentPane();
         pane.setLayout(new BorderLayout());
@@ -20,6 +45,23 @@ public class MainUI {
                 // rendering magic will happen here
             }
         };
+
+        renderPanel.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                double yi = 180.0 / renderPanel.getHeight();
+                double xi = 180.0 / renderPanel.getWidth();
+                x[0] = (int) (e.getX() * xi);
+                y[0] = -(int) (e.getY() * yi);
+                renderPanel.repaint();
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+            }
+        });
+
         pane.add(renderPanel, BorderLayout.CENTER);
 
         frame.setSize(600, 600);
